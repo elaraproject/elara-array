feae70453ae7ba4a78c6a7e98519f65084449089
Jacky Song <almey0721@gmail.com>
2023-07-21 15:16:05 +0800

    Improved doc comments for lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+/// An n-dimensional data container, similar
+/// to a NumPy array
 pub mod array;
+/// Various useful constants and numerical
+/// functions
 pub mod num;
 
 pub use array::NdArray;
